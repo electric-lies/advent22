89de66a088103d29f5e85ce0ecfaac8be87737a8
electric lies <electric.lies@proton.me>
2022-12-02 22:43:46 +0200

    itertools

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
@@ -5,22 +6,14 @@ fn main() -> io::Result<()> {
     let file = File::open("src/input.txt")?;
     let reader = BufReader::new(file);
 
-    let mut curr_sum = 0;
-    let mut sums = Vec::new();
+    let mut sums = reader
+        .lines()
+        .filter_map(|x| x.ok())
+        .group_by(|content| content.is_empty())
+        .into_iter()
+        .map(|(_, l)| l.into_iter().map(|s| s.parse::<i32>().unwrap_or(0)).sum())
+        .collect::<Vec<i32>>();
 
-    for line in reader.lines() {
-        match line {
-            Ok(content) => {
-                if content.is_empty() {
-                    sums.push(curr_sum);
-                    curr_sum = 0
-                } else {
-                    curr_sum += content.parse::<i32>().unwrap();
-                }
-            }
-            Err(_) => todo!(),
-        }
-    }
     sums.sort();
     sums.reverse();
     println!("{:?}, {}", sums, sums[0..3].iter().sum::<i32>());
